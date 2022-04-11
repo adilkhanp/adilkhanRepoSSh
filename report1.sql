069cfac328a21c5132ceaa3f77f7d703b0a4cb6b
adilkhanpattan <adilkhan.p1993@gmail.com>
2022-04-11 06:28:21 +0530

    Removed count display after loop and display ename inside loop

--- report1.sql
+++ report1.sql
@@ -10,9 +10,8 @@ loop
 fetch data_cur into lv_ename;
 exit when data_cur%notfound;
 
-dbms_output.put_line(data_cur%rowcount);
+dbms_output.put_line(lv_ename);
 
 end loop;
-dbms_output.put_line(data_cur%rowcount);
 close data_cur;
 end;
