dc536be0d428b43431959c2fd6582c8dc01bb6c5
adilkhanpattan <adilkhan.p1993@gmail.com>
2022-04-11 06:36:47 +0530

    Added line to display Salary in new line

--- report1.sql
+++ report1.sql
@@ -1,8 +1,9 @@
 declare
 cursor data_cur is
-select ename from emp;
+select ename,sal from emp;
 
 lv_ename emp.ename%type;
+lv_sal   emp.sal%type;
 
 begin
 open data_cur;
@@ -11,6 +12,7 @@ fetch data_cur into lv_ename;
 exit when data_cur%notfound;
 
 dbms_output.put_line(lv_ename);
+dbms_output.put_line(lv_sal);
 
 end loop;
 close data_cur;
